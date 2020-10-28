e174c46bab2c46686f6830d46a07def708a3484b
Tw2Elven <herickvmaim@outlook.com>
2020-10-28 15:52:08 -0300

    mudancas no display da biografia

--- public/script/biografiaChamada.js
+++ public/script/biografiaChamada.js
@@ -3,13 +3,21 @@ const bioPainel = document.querySelector('.biografia')
 const gatilhoBiografia = document.querySelector('.circular')
 
 gatilhoFechar.addEventListener('click', () => {
+    bioPainel.style.display = 'none'
+    /*
     bioPainel.style.opacity = '0'
     bioPainel.style.zIndex = '-1'
+    */
+   document.body.style.overflow = 'unset'
+
 })
 
 gatilhoBiografia.addEventListener('click', () => {
+    bioPainel.style.display = 'block'
+    /*
     bioPainel.style.opacity = '0.9'
     bioPainel.style.zIndex = '1'
+    */
     document.body.style.overflow = 'hidden'
 })
 
